665c25d4727ef9fb7916ec0dad806fa379d1a1f6
nelanz <n.tomaszewicz@gmail.com>
2019-08-01 18:19:02 +0200

    Added chart bars

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import './chart_bar.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -7,16 +8,17 @@ class Chart extends StatelessWidget {
 
   Chart(this.recentTransactions);
 
-  List <Map<String, Object>> get groupedTransactionValues {
-
+  List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(Duration(days: index),);
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
       var totalSum = 0.0;
 
-      for(var i = 0; i < recentTransactions.length; i++) {
-        if(recentTransactions[i].date.day == weekDay.day
-            && recentTransactions[i].date.month == weekDay.month
-            && recentTransactions[i].date.year == weekDay.year) {
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
           totalSum += recentTransactions[i].amount;
         }
       }
@@ -24,9 +26,15 @@ class Chart extends StatelessWidget {
       print(totalSum);
 
       return {
-        'day' : DateFormat.E().format(weekDay), 'amount' : totalSum
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
       };
     });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(
+        0.0, (sum, item) => sum + item['amount']);
   }
 
   @override
@@ -35,10 +43,18 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: <Widget>[
-
-        ],
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) =>
+              ChartBar(
+                  data['day'],
+                  data['amount'],
+                  totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending)
+          )
+              .toList(),
+        ),
       ),
     );
   }
